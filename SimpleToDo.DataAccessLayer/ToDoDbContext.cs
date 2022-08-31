3623c4cd667631bd6be7e2ecb973ac011314b5b6
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2022-09-01 00:26:00 +0430

    init db context

---
+++ SimpleToDo.DataAccessLayer/ToDoDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+using SimpleToDo.StorageModels;
+
+namespace SimpleToDo.DataAccessLayer;
+
+public class ToDoDbContext : DbContext
+{
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Company> Companies { get; set; }
+    public DbSet<CompanyGroup> CompanyGroups { get; set; }
+    public DbSet<CompanyUser> CompanyUsers { get; set; }
+    public DbSet<Group> Groups { get; set; }
+    public DbSet<User> Users { get; set; }
+    public DbSet<GroupUser> GroupUsers { get; set; }
+    public DbSet<GroupToDo> GroupToDos { get; set; }
+    public DbSet<UserToDo> UserToDos { get; set; }
+    public DbSet<ToDo> ToDos { get; set; }
+}
