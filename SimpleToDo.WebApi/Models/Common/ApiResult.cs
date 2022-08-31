0e2c61f087c90ace25fd7c4d9554883ff1321af5
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2022-09-01 01:20:16 +0430

    add api resul model

---
+++ SimpleToDo.WebApi/Models/Common/ApiResult.cs
@@ -0,0 +1,33 @@
+﻿namespace SimpleToDo.WebApi.Models.Common
+{
+    public class ApiResult<T>
+    {
+        private ApiResult()
+        {
+        }
+
+        public T? Result { get; set; }
+        public string? Error { get; set; }
+        public bool Success { get; set; }
+
+        public static ApiResult<T> AlreadyExists { get; } = new ApiResult<T> { Error = "Item already exists" };
+        public static ApiResult<T> NotFound { get; } = new ApiResult<T> { Error = "Item not found" };
+
+        public static ApiResult<T> CreateSuccessResult(T? result)
+        {
+            return new ApiResult<T>
+            {
+                Success = true,
+                Result = result
+            };
+        }
+
+        public static ApiResult<T> CreateErrorResult(Exception ex, string? message = null)
+        {
+            return new ApiResult<T>
+            {
+                Error = message ?? ex.Message
+            };
+        }
+    }
+}
